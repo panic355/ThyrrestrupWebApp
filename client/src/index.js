6ade362aafceb60fe28439324f2eae7858b7fc4d
Panic <mart274f@gmail.com>
2020-11-17 18:32:03 +0100

    added missing modules

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
-import { render } from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
-
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App/App';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import { CookiesProvider } from 'react-cookie';
+import 'bootstrap/dist/css/bootstrap.css';
 
-render((
-    <BrowserRouter>
-        <App/>
-    </BrowserRouter>
-), document.getElementById('root'));
+ReactDOM.render(
+  <CookiesProvider >
+  <App/>
+</CookiesProvider>,
+  /*<React.StrictMode>
+    <App />     Måske virker det her, fixede det som faktisk var fejlen ved cookies
+  </React.StrictMode>,*/
+  document.getElementById('root')
+);
 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
