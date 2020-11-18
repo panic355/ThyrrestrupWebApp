c219520c481cd0b60d301d59d59d53bf49600555
Panic <mart274f@gmail.com>
2020-11-18 13:30:30 +0100

    small changes to GUI

--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -13,8 +13,8 @@ import ContactUs from '../contactPage/ContactCard';
 import CreateMachine from '../createMachine/CreateMachine';
 import Login from '../Login/LoginForm';
 import FleetVehicles from '../fleet/FleetVehicles';
-//import logo from './logo.png'
-const logo = require('./logoWhite.svg');
+import logo from './logoWhite.svg'
+//const logo = require('./logoDark.png');
 class BootstrapNavbar extends React.Component {
     render() {
         return (
@@ -29,20 +29,19 @@ class BootstrapNavbar extends React.Component {
                                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                                 <Navbar.Collapse id="basic-navbar-nav">
                                     <Nav className="mr-auto">
-                                        <Nav.Link href="/">Home</Nav.Link>
-                                        <Nav.Link href="/register">Register</Nav.Link>
-                                        <Nav.Link href="/contact">Contact Us</Nav.Link>
+                                        <Nav.Link href="/">Hjem</Nav.Link>
                                         <Nav.Link href="/fleet">Flåde</Nav.Link>
-                                        <Nav.Link href="/contact-us">About Us</Nav.Link>
-                                        <Nav.Link href="/login">Login</Nav.Link>
-                                        <Nav.Link href="/createMachine">CreateMachine</Nav.Link>
-                                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                                        <Nav.Link href="/contact-us">Om os</Nav.Link>
+                                        <Nav.Link href="/createMachine">Lav maskine</Nav.Link>
+                                        {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
                                             <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
                                             <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
                                             <NavDropdown.Divider />
                                             <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                        </NavDropdown>
+                                        </NavDropdown> */}
+                                        <Nav.Link href="/login">Login</Nav.Link>
+                                        <Nav.Link href="/register">Register</Nav.Link>
                                     </Nav>
                                     <Form inline>
                                         <FormControl type="text" placeholder="Search" className="mr-sm-2" />
