9d1375bda70003666535c38c72b6ecc7e17a9d17
Panic <mart274f@gmail.com>
2020-11-18 09:27:56 +0100

    test

--- index.js
+++ index.js
@@ -42,9 +42,6 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-});
 
 mssql.connect(config, function (error) {
   if (error) {
