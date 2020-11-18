8042d05ac4e36fd3f91da19df2f3f7354bc4ec54
Chris <manchristoffer@yahoo.dk>
2020-11-18 14:18:43 +0100

    Silly test

--- index.js
+++ index.js
@@ -54,11 +54,16 @@ mssql.connect(config, function (error) {
   }
 });
 
-
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 
 app.use('/', require('./routes/pages'));
 app.use('/auth', require('./routes/auth'));
 
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "client/build", "index.html"));
+});
 
 app.listen(PORT, console.log(`server is starting at ${PORT}`));
