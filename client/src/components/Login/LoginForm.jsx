af548ab150e36ed766f67488031f1bdfe01cf072
Veylas <manchristoffer@yahoo.dk>
2020-12-14 08:38:45 +0100

    Deleted unued code

--- client/src/components/Login/LoginForm.jsx
+++ client/src/components/Login/LoginForm.jsx
@@ -55,10 +55,6 @@ handleSubmit(e) {
         const { email, password, submitted, loading, error } = this.state;
         return (
             <div className="col-md-6 col-md-offset-3">
-                <div className="alert alert-info">
-                    Email: test<br />
-                    Password: test
-                </div>
                 <h2>Login</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
