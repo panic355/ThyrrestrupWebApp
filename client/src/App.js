027ea590fb7330dc80763b0f907a1faeab00cce7
Veylas <manchristoffer@yahoo.dk>
2020-12-18 09:00:55 +0100

    Changed a name to start with capital letter C

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Navbar from "./components/navbar/Navbar";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
-import cookiePermission from "./components/CookiePermision"
+import CookiePermission from "./components/CookiePermision"
 
 class App extends Component {
   
@@ -18,14 +18,14 @@ class App extends Component {
   render() {
 
     return (
-      <cookiePermission>
+      <CookiePermission>
       <Router>
         <Navbar />
         <Switch>
           <Route exactly pattern="/" />
         </Switch>
       </Router>
-      </cookiePermission>
+      </CookiePermission>
       );
   }
 }
